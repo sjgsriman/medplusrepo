1646eecc971c3d614d3d1f71c59484bc695578d0
sjgsriman <sjg.sriman@gmail.com>
2018-08-12 21:13:17 +0530

    added this is git

--- access/src/main/java/apollo/com/App.java
+++ access/src/main/java/apollo/com/App.java
@@ -4,10 +4,9 @@ package apollo.com;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("This is git");
+	}
 }
